cccf313cdb2bc5eb824c718ebe11d0109b1844bc
udaykumar1021 <110233587+udaykumar1021@users.noreply.github.com>
2025-11-18 15:14:57 +0530

    Add files via upload

---
+++ guessing_game_computer.py
@@ -0,0 +1,15 @@
+import random
+
+def guess(x):
+    random_number=random.randint(1,x)
+    guess=0
+    while guess!=random_number:
+        guess=int(input(f"guess the number between 1 and {x}: "))
+        if guess<random_number:
+            print("sorry,number is too low")
+        elif guess>random_number:
+            print("sorry,number is too high")
+        
+    print(f"yay!,congrats you have guessed the number {random_number},correctly")
+
+guess(15)
